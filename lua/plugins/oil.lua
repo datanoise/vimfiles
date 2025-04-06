5888cc5bb3b23b70b8a0243a4ff65ce601d8dc43
Kent Sibilev <ksibilev@gmail.com>
2025-04-05 23:57:51 -0400

    added oil floater

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,17 @@ if not vim.g.plugs['oil.nvim'] then
     return
 end
 
-require("oil").setup()
+require("oil").setup({
+    float = {
+        padding = 2,
+        max_width = 0.5,
+        max_height = 0.5,
+        border = "rounded",
+    },
+    keymaps = {
+        ["q"] = { "actions.close", mode = "n" }
+    }
+})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "g-", function() require'oil'.toggle_float() end, { desc = "Toggle parent directory" })
